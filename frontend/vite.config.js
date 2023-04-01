f6cc702f9286f4d504ebfeeac7072186a94b17aa
Adil Mohammed <75264739+adil200@users.noreply.github.com>
2023-04-01 21:36:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// vite.config.js
+
+export default {
+  build: {
+    rollupOptions: {
+      external: ['/src/main.jsx']
+    }
+  }
+}
